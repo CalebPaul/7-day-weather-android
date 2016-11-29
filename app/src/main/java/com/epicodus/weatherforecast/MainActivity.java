4b1d3d0c4150d91d668cc1da2e536f58e806d3bb
Caleb Paul and Tim Jung <caleb@>
2016-11-29 14:53:31 -0800

    add weather activity, and add button to change activities

--- app/src/main/java/com/epicodus/weatherforecast/MainActivity.java
+++ app/src/main/java/com/epicodus/weatherforecast/MainActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.weatherforecast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.submitLocationButton) Button mSubmitLocationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSubmitLocationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSubmitLocationButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
